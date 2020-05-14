0c1535f2573cd99474d243c9c262e519530539a5
PEROOT\VGowdSa <sachin.gowdar@pearson.com>
2020-05-14 10:16:55 +0530

    docker changed

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM java:8-jdk-alpine
 RUN ["ls"]
-COPY ./target/playround-0.0.1-SNAPSHOT.jar /usr/app/
-WORKDIR /usr/app
+ADD ./target/playround-0.0.1-SNAPSHOT.jar playround-0.0.1-SNAPSHOT.jar
 EXPOSE 8083
 ENTRYPOINT ["java", "-jar", "playround-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/playround/sayhello/controller/MainController.java
+++ src/main/java/com/playround/sayhello/controller/MainController.java
@@ -3,13 +3,24 @@ package com.playround.sayhello.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-	@GetMapping(value = { "/sayhello", "/sayhello/{name}" })
+	@GetMapping(value = { "/" })
 	public ResponseEntity<String> sayHello(@PathVariable(name = "name",required = false ) String name) {
+		String statement = null;
+		if (name == null)
+			statement = "Hello!!";
+		else
+			statement = "Hello! " + name+ "...";
+		return ResponseEntity.ok(statement);
+	}
+	
+	@GetMapping(value = { "/sayhello", "/sayhello/{name}" })
+	public ResponseEntity<String> sayHelloAgain(@PathVariable(name = "name",required = false ) String name) {
 		String statement = null;
 		if (name == null)
 			statement = "Hello!!";
